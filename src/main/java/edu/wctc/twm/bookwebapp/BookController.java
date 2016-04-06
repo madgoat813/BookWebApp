a7fbd25d5edec8e58450218eb24349608e79ee70
madgoat813 <mcfall86@gmail.com>
2016-04-06 18:00:55 -0500

    more book fixes

--- src/main/java/edu/wctc/twm/bookwebapp/BookController.java
+++ src/main/java/edu/wctc/twm/bookwebapp/BookController.java
@@ -1,12 +1,22 @@
+package edu.wctc.twm.bookwebapp;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.wctc.twm.bookwebapp;
+import edu.wctc.twm.bookwebapp.ejb.AbstractFacade;
+import edu.wctc.twm.bookwebapp.ejb.BookFacade;
+import edu.wctc.twm.bookwebapp.model.Author;
+import edu.wctc.twm.bookwebapp.model.Book;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +30,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "BookController", urlPatterns = {"/BookController"})
 public class BookController extends HttpServlet {
 
+    private static final String NO_PARAM_ERR_MSG = "No request parameter identified";
+    private static final String LIST_PAGE = "/listBooks.jsp";
+    private static final String ADD_PAGE = "/bookAddPage.jsp";
+    private static final String EDIT_PAGE = "/bookEditPage.jsp";
+    private static final String EDIT_ERROR = "/testPage.jsp";
+    private static final String LIST_ACTION = "list";
+    private static final String ADD_EDIT_DELETE_ACTION = "addEditDelete";
+    private static final String SUBMIT_ACTION = "submit";
+    private static final String ACTION_PARAM = "action";
+    private static final String SAVE_ACTION = "save";
+    private static final String ADD_ACTION = "add";
+    private static final String CANCEL_ACTION = "cancel";
+
+    private String driverClass;
+    private String url;
+    private String userName;
+    private String password;
+    private String dbJndiName;
+
+    @Inject
+    private BookFacade bServe;
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -28,22 +60,125 @@ public class BookController extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     * @throws java.lang.ClassNotFoundException
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, ClassNotFoundException, Exception {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet BookController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet BookController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        String destination = LIST_PAGE;
+        String action = request.getParameter(ACTION_PARAM);
+
+        // use init parameters to config database connection
+        //configDbConnection();
+
+        try {
+            // use init parameters to config database connection
+            //configDbConnection();
+            
+            OUTER:
+            switch (action) {
+                case LIST_ACTION:
+                    this.refreshList(request, bServe);
+                    destination = LIST_PAGE;
+                    break;
+                case ADD_EDIT_DELETE_ACTION:
+                    String subAction = request.getParameter(SUBMIT_ACTION);
+                    switch (subAction) {
+                        case "new": {
+                            // must be add or edit, go to addEdit page
+                            String[] bookIds = request.getParameterValues("bookId");
+                            destination = ADD_PAGE;
+                            break;
+                        }
+                        case "edit": {
+                            String[] bookIds = request.getParameterValues("bookId");
+                            if (bookIds == null || bookIds.length > 1) {
+                                this.refreshList(request, bServe);
+                                destination = EDIT_ERROR;
+                                break OUTER;
+                            }
+                            String bookId = bookIds[0];
+                            Book book = bServe.find(bookId);
+                            request.setAttribute("book", book);
+                            destination = EDIT_PAGE;
+                            break;
+                        }
+                        default: {
+                            // must be DELETE
+                            // get array based on records checked
+                            String[] bookIds = request.getParameterValues("bookId");
+                            for (String id : bookIds) {
+                                Book b =  bServe.find(id);
+                                bServe.remove(b);
+                            }
+                            this.refreshList(request, bServe);
+                            destination = LIST_PAGE;
+                            break;
+                        }
+                    }
+                    break;
+                case SAVE_ACTION:
+                    String bookName = request.getParameter("bookName");
+                    String bookId = request.getParameter("bookId");
+                    String isbn = request.getParameter("isbn");
+                    String authorId = request.getParameter("authorId");
+                    Book book = bServe.find(new Integer(bookId));
+                        book.setBookName(bookName);
+                        book.setIsbn(isbn);
+                        book.setAuthorId(new Author(Integer.parseInt(authorId)));
+                    bServe.edit(book);
+                    this.refreshList(request, bServe);
+                    destination = LIST_PAGE;
+                    break;
+                case ADD_ACTION:
+                    String bName = request.getParameter("bookName");
+                    String bIsbn = request.getParameter("isbn");
+                    String bAuthorId = request.getParameter("authorId");
+                    book = new Book();
+                        book.setBookName(bName);
+                        book.setIsbn(bIsbn);
+                        book.setAuthorId(new Author(Integer.parseInt(bAuthorId)));
+                    bServe.create(book);
+                    this.refreshList(request, bServe);
+                    destination = LIST_PAGE;
+                    break;
+                case CANCEL_ACTION:
+                    this.refreshList(request, bServe);
+                    destination = LIST_PAGE;
+                    break;
+                default:
+                    // no param identified in request, must be an error
+                    request.setAttribute("errMsg", NO_PARAM_ERR_MSG);
+                    destination = "/testPage.jsp";
+                    break;
+            }
+
+        } catch (Exception e) {
+
         }
+
+        // Forward to destination page
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(destination);
+
+        dispatcher.forward(request, response);
+
+    }
+
+//    private void configDbConnection() throws NamingException, ClassNotFoundException, SQLException {
+//        if (dbJndiName == null) {
+//            aServe.getDao().initDao(driverClass, url, userName, password);
+//        } else {
+//            Context ctx = new InitialContext();
+//            DataSource ds = (DataSource) ctx.lookup(dbJndiName);
+//            aServe.getDao().initDao(ds);
+//        }
+//    }
+
+    private void refreshList(HttpServletRequest request, AbstractFacade bServe) throws Exception {
+        List<Book> books = bServe.findAll();
+        request.setAttribute("books", books);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -58,7 +193,21 @@ public class BookController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+
+        } catch (Exception ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -72,7 +221,21 @@ public class BookController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+
+        } catch (Exception ex) {
+            Logger.getLogger(AuthorController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -85,4 +248,12 @@ public class BookController extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    @Override
+    public void init() throws ServletException {
+//        driverClass = getServletContext().getInitParameter("db.driver.class");
+//        url = getServletContext().getInitParameter("db.url");
+//        userName = getServletContext().getInitParameter("db.username");
+//        password = getServletContext().getInitParameter("db.password");
+        dbJndiName = getServletContext().getInitParameter("db.jndi.name");
+    }
 }
---
+++ src/main/webapp/bookEditPage.jsp
@@ -0,0 +1,57 @@
+<%-- 
+    Document   : bookEditPage
+    Created on : Apr 6, 2016, 5:51:40 PM
+    Author     : Taylor
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Edit Book</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+              rel="stylesheet" 
+              integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" 
+              crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Edit Book</h1>
+
+        <form method="POST" action="BookController" class="col-md-4">
+            <br/>
+            <div class="alert alert-info" role="alert">Edit Book</div>
+            <table class="table">
+                <tr>
+                <thead><td>ID</td></thead>
+                <td><input type="text" name="bookId" value="${book.bookId}" class="form-control" readonly/></td>
+                </tr>
+                <tr>
+                <thead><td>Name</td></thead>
+                <td><input type="text" name="bookName" value="${book.bookName}" class="form-control"/></td>
+                </tr>
+                <tr>
+                <thead><td>Book Isbn</td></thead>
+                <td><input type="text" name="isbn" value="${book.isbn}" class="form-control"/></td>
+                </tr>
+                <tr>
+                <thead><td>Author Id</td></thead>
+                <td><input type="text" name="authorId" value="${book.authorId}" class="form-control"/></td>
+                </tr>
+                <tr>
+                    <input type="submit" value="save" name="action" class="btn btn-primary"/>&nbsp;
+                    <input type="submit" value="cancel" name="action" class="btn btn-danger"/>
+                </tr>
+            </table>
+                
+            
+
+
+
+        </form>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" 
+                integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" 
+        crossorigin="anonymous"></script>
+    </body>
+</html>
